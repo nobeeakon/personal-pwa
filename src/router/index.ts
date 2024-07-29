1038359dbd66aaed59c5a0757d4bd92ad9008b5b
Daniel Torres <nobeeakon@gmail.com>
2024-07-29 12:23:05 -0600

    update to hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomePage from '../pages/HelloWorld.vue'
 import ShopList from '../pages/shop-list-page/ListPage.vue'
 import TimelinePage from '../pages/timeline/Timeline.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path:"/",
